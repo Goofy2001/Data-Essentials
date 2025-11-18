1c87890540dae0a77981bfc1c5d61d636a438ce3
Goofy2001 <thibo.coudron@gmail.com>
2025-11-18 16:08:27 +0100

    initial push

---
+++ basis_SELECT.sql
@@ -0,0 +1,44 @@
+SELECT LEV_NAAM
+FROM LEVERANCIERS
+ORDER BY LEV_NAAM DESC;
+
+SELECT PLANTENNAAM
+FROM PLANTEN
+WHERE SOORT LIKE 'vast'
+ORDER BY PLANTENNAAM DESC;
+
+SELECT LEV_NAAM, WOONPLAATS
+FROM LEVERANCIERS;
+
+select lev_naam, woonplaats
+from leveranciers;
+
+SELECT LEV_NAAM || 'is gevestigd in ' || WOONPLAATS AS LEVERANCIERSINFORMATIE
+FROM LEVERANCIERS;
+
+SELECT lev_code, lev_naam
+FROM LEVERANCIERS
+WHERE lev_naam LIKE '%o%' OR lev_naam LIKE 'O%';
+
+SELECT lev_naam
+FROM LEVERANCIERS
+WHERE WOONPLAATS LIKE 'Aalsmeer' AND korting >= 5;
+
+SELECT plantennaam, prijs
+FROM PLANTEN
+WHERE prijs < 5;
+
+SELECT art_code, plantennaam, SOORT
+FROM PLANTEN
+where soort LIKE 'vast' OR soort LIKE 'boom'
+ORDER BY plantennaam;
+
+SELECT *
+FROM PLANTEN
+WHERE hoogte BETWEEN 100 AND 200;
+
+SELECT *
+FROM PLANTEN
+WHERE soort LIKE 'water'
+ORDER BY hoogte;
+
